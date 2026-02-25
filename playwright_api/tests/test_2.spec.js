3a058159396fc8197129ed7a825e4c91837dcb07
Vladyslav-Riabets <ryabets.vlad@gmail.com>
2026-02-25 15:24:02 +0200

    added negative cases

--- playwright_api/tests/test_2.spec.js
+++ playwright_api/tests/test_2.spec.js
@@ -15,4 +15,32 @@ test.describe('TASK 2: car tests', () => {
         expect(car.data.brand).toBe('Audi');
         expect(car.data.model).toBe('TT');
     });
+
+    test('NEGATIVE 1: should check error message: Car brand id is required', async ({request}) => {
+        const response = await request.post('/api/cars', {
+            data: {
+                carModelId: 1,
+                mileage: 1,               
+            },
+        });
+
+        expect(response.status()).toBe(400);
+        const error = await response.json();
+        expect(error.message).toBe('Car brand id is required');
+    });
+
+    test('NEGATIVE 2: should check error for unexisted car brand', async ({request}) => {
+        const response = await request.post('/api/cars', {
+            data: {
+                carBrandId: 7,
+                carModelId: 1,
+                mileage: 1,
+            },
+        });
+
+        expect(response.status()).toBe(404);
+        const error = await response.json();
+        expect(error.message).toBe('Brand not found');
+    });
+
 });
