7ba0e6266a7339f9121588a99ffb3aa40205ce45
Vladyslav-Riabets <ryabets.vlad@gmail.com>
2026-02-26 14:32:47 +0200

    fix test

--- playwright_api/tests/test_1.spec.js
+++ playwright_api/tests/test_1.spec.js
@@ -1,27 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('TASK 1: user data substitution', async ({ page }) => {
-  const userData = {
-    userId: 123456,
-    photoFilename: 'default-user.png',
-    name: 'John',
-    lastName: 'Smith',
+  const mockResponse = {
+    statis: 'ok',
+    data: {
+      userId: 123456,
+      photoFilename: 'default-user.png',
+      name: 'John',
+      lastName: 'Smith',
+    }
   };
 
-  await page.route('**/api/users/profile', async (route) => {
-    await route.fulfill({
+  await page.route('**/api/users/profile', (route) => {
+    route.fulfill({
       status: 200,
-      json: userData,
+      contentType: 'application/json',
+      body: JSON.stringify(mockResponse),
     });
   });
 
-  const responsePromise = page.waitForResponse('**/api/users/profile');
-
   await page.goto('/panel/profile');
-
-  const response = await responsePromise;
-  const json = await response.json();
-
-  expect(json.name).toBe('John');
-  expect(json.lastName).toBe('Smith');
+  await expect(page.locator('p.profile_name')).toHaveText('John Smith');
 });
